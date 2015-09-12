49d10a7c122a7d810a728adb9374268bc2b7870b
Spencer Hawkins <hawkins.spencer@gmail.com>
2015-09-12 17:31:12 -0500

    use correct bash conditional, virtualenv path

--- ci/test.sh
+++ ci/test.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 killall python # in case any processes were left dangling
 set -e # exit with non-zero exit codes immediately
-if [ -z "$local_instance" ]; then
+if [ "$local_instance" = "yes" ]; then
     # this is on a server
     . ~/gitsubmitenv/bin/activate
 else
     # This is probably being run locally by shawkins
-    . /virtualenv/gitsubmit_env/bin/activate
+    . /virtualenvs/gitsubmit_env/bin/activate
 fi
 
-cd ..
 python src/app.py -p 5123 &
 sleep 3 # let tornado warm up
 cd test
