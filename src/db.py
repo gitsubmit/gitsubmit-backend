3344ca22dc55a0c50e52509949879b36a17be6a5
Tsintsir <tsintsir@gmail.com>
2015-09-14 00:23:40 -0500

    Rough outline of create_user. Not functional.

---
+++ src/db.py
@@ -0,0 +1,19 @@
+__author__ = 'tsirist'
+
+from bson import Binary
+from os import urandom
+from pymongo import MongoClient
+from pbkdf2 import PBKDF2
+
+def create_user(username, email, password):
+    client = MongoClient()
+    db = client.gitsubmit.users
+    salt = urandom(256)
+    db.insert_one(
+        {
+            "username": username,
+            "email":    email,
+            "salt":     salt,
+            "hash":     Binary.new(PBKDF2(password, salt).read(256))    # TODO: This doesn't store successfully; expects a string
+        }
+    )
