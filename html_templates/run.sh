0626dfba76a263dbe744f4608e09d7fa8ae7b495
Son Pham <sonpa1993@gmail.com>
2015-09-13 20:27:24 -0500

    update run script

--- html_templates/run.sh
+++ html_templates/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python -m SimpleHTTPServer
+git pull && git submodule init && git submodule update && git submodule status
+if which python 2>&1 > /dev/null; then
+    python -m SimpleHTTPServer
+else
+    echo 'Python is not installed'
+fi
