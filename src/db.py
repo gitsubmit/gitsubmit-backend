330bb74017c44bf6b539ab369847ef6f0c59225c
Tsintsir <tsintsir@gmail.com>
2015-09-17 20:45:51 -0500

    Forgot to stage this file.

--- src/db.py
+++ src/db.py
@@ -1,9 +1,10 @@
 __author__ = 'Tsintsir'
 
-from bson import Binary
+from binascii import b2a_hex
 from os import urandom
 from pymongo import MongoClient
 from pbkdf2 import PBKDF2
+
 
 def create_user(username, email, password):
     client = MongoClient()
@@ -14,6 +15,20 @@ def create_user(username, email, password):
             "username": username,
             "email":    email,
             "salt":     salt,
-            "hash":     Binary.new(PBKDF2(password, salt).read(256))    # TODO: This doesn't store successfully; expects a string
+            "hash":     b2a_hex(PBKDF2(password, salt).read(256))
         }
     )
+
+
+def login(username, password):
+    client = MongoClient()
+    db = client.gitsubmit.users
+    user_doc = db.find_one({"username": username})
+    if user_doc is None:
+        raise LookupError("No such username could be found.")
+    else:
+        salt = user_doc["salt"]
+        if b2a_hex(PBKDF2(password, salt).read(256)) == user_doc["hash"]:
+            return 1
+        else:
+            raise StandardError("Incorrect password.")
