0e05985ddb429f17ad1951091906580c1c3c97a0
Spencer Hawkins <hawkins.spencer@gmail.com>
2015-11-18 18:42:57 -0600

    use a non-80 port

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,7 +9,7 @@ git submodule update
 git submodule status
 pip install -r requirements.txt # get python packages
 cd src
-nohup /virtualenvs/gitsubmit_env/bin/gunicorn --access-logfile /srv/logs/access.log -w 10 -b :80 app:app &
+nohup /virtualenvs/gitsubmit_env/bin/gunicorn --access-logfile /srv/logs/access.log -w 10 -b :5124 app:app &
 # -w = number workers, -b = socket to bind (blank ip = broadcast, 0.0.0.0, port = 80)
 sleep 3
 curl http://gitsubmit.com/
